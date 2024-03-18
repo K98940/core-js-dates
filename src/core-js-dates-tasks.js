d7163bff5d17f524557166d500c4ff1482cd0ad7
k98940 <pv1xxb@yandex.ru>
2024-03-18 23:08:44 +0600

    feat: getQuarter, isLeapYear

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -278,8 +278,8 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  return Math.ceil((date.getMonth() + 1) / 3);
 }
 
 /**
@@ -316,8 +316,11 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const BUT_NOT_100 = 100;
+  const year = date.getFullYear();
+  if (year === BUT_NOT_100) return false;
+  return year % 4 === 0;
 }
 
 module.exports = {
